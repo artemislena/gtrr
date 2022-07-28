5aa098e345966d9f4dee067f3ee65a77d0607716
artemislena <artemis@artemislena.eu>
2022-07-28 11:55:57 +0200

    L: Fixed find usage in the deploy script.

--- _deploy.sh
+++ _deploy.sh
@@ -4,11 +4,11 @@
 
 cobalt build
 
-find _site/ -name '*.css' -exec css-html-js-minify --quiet --overwrite {} \; && echo 'CSS minified.'
+find _site -name '*.css' -exec css-html-js-minify --quiet --overwrite {} \; && echo 'CSS minified.'
 
 # Ensure correct permissions
-find _site/ -type f -exec chmod 644 {} \;
-find _site/ -type d -exec chmod 755 {} \;
+find _site -type f -exec chmod 644 {} \;
+find _site -type d -exec chmod 755 {} \;
 
 echo 'Touch YubiKey'
 rsync -a --progress --delete _site/ root@192.168.100.80:/srv/www/gtrr/
