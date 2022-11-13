bcf5be6370437d5dd7ef55a9d3e6863c80594d3d
artemislena <artemis@artemislena.eu>
2022-11-13 18:45:59 +0100

    T.: Deploy script update

--- _deploy.sh
+++ _deploy.sh
@@ -17,6 +17,6 @@ find _site -type f -exec chmod 644 {} \;
 find _site -type d -exec chmod 755 {} \;
 
 echo 'Touch YubiKey'
-rsync -a --progress --delete _site/ root@192.168.100.80:/srv/www/gtrr/
+rsync -a --progress --delete _site/ http:/srv/www/gtrr/
 
 rm -r _site/ && echo 'Build files removed.'
