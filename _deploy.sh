7c4d32526137e2a31841b011dce1643bf3e1a85d
artemislena <artemis@artemislena.eu>
2023-04-19 19:51:35 +0200

    L: Uhm, changed compression levels

--- _deploy.sh
+++ _deploy.sh
@@ -17,7 +17,7 @@ find _site -name '*.css' -exec css-html-js-minify --quiet --overwrite {} \;
 echo 'CSS minified.'
 
 echo 'Precompressing…'
-find -E _site -type f -not -regex '.*\.(png|zip|7z|jpeg|webp|br|zst|gz|html)' -exec zstd -q {} \; -exec gzip -k {} \; -exec brotli {} \;
+find -E _site -type f -not -regex '.*\.(png|zip|7z|jpeg|webp|br|zst|gz|html)' -exec zstd -q -19 {} \; -exec gzip -k --best {} \; -exec brotli {} \;
 
 chmod -R a+rX _site
 
