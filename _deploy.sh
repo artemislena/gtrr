956ceb69ad6fda815fb651f03ac5d52e576f138c
artemislena <artemis@artemislena.eu>
2023-07-20 21:09:05 +0200

    fc17: More… compatibility with… other environments…

--- _deploy.sh
+++ _deploy.sh
@@ -10,7 +10,7 @@ echo 'Calculating hashes…'
 find static -type d -exec mkdir -p _data/hashes/{} \; >/dev/null
 # This one replaces "." with "-" and prepends it if filename starts with a digit
 find static -type f -not -name '.DS_Store' \
--exec sh -c 'echo "\"$(xxh32sum "$1" | cut -d " " -f 1 | xxd -r -p | base64 | sed s/=//g | tr +/ -_)\"" > "_data/hashes/$(dirname "$1")/$(basename "$1" | cut -c 1 | sed "s/[[:digit:]]/-/" | grep "\-")$(basename "$1" | tr . -).json"' sh {} \;
+-exec sh -c 'echo "\"$(xxhsum -H0 "$1" | cut -d " " -f 1 | xxd -r -p | base64 | sed s/=//g | tr +/ -_)\"" > "_data/hashes/$(dirname "$1")/$(basename "$1" | cut -c 1 | sed "s/[[:digit:]]/-/" | grep "\-")$(basename "$1" | tr . -).json"' sh {} \;
 
 cobalt build
 
@@ -18,7 +18,7 @@ find _site -name '*.css' -exec css-html-js-minify --quiet --overwrite {} \;
 echo 'CSS minified.'
 
 echo 'Precompressing…'
-find -E _site -type f -not -regex '.*\.(png|zip|7z|jpeg|webp|br|gz|zst|html)' -not -size '1' -exec gzip -k --best {} \; -exec brotli {} \;
+find -E _site -type f -not '(' -regex '.*\.(png|zip|7z|jpeg|webp|br|gz|zst|html)' -or -size 1 ')' -exec gzip -k --best {} \; -exec brotli {} \;
 
 chmod -R a+rX _site
 
